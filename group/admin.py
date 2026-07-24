3ede27aa377dbc49b37bb0662cbebef9d5b8ab86
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-24 12:04:54 +0530

    done with group models creation and wiring too

--- group/admin.py
+++ group/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from group.models import Group, GroupMessage
 
-# Register your models here.
+admin.site.register(Group)
+admin.site.register(GroupMessage)
--- group/models.py
+++ group/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+    name=models.CharField(max_length=150)
+    about=models.CharField(max_length=500)
+    user=models.ManyToManyField(User, unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class GroupMessage(models.Model):
+    group=models.ForeignKey(Group, on_delete=models.CASCADE, related_name='group_messages')
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    message=models.TextField()
+    time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.group.name
