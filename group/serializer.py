09bbddf8d560718a98e7c48b5121142ac7fcd158
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-24 12:59:38 +0530

    added all the serializers

---
+++ group/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework.serializers import ModelSerializer
+from group.models import Group, GroupMessage
+from whoisthis.serializers import UserSerializer
+
+class GroupSerializer(ModelSerializer):
+    user=UserSerializer(read_only=True, many=True)
+    class Meta:
+        model=Group
+        fields='__all__'
+
+
+class GroupMessageSerializer(ModelSerializer):
+    user=UserSerializer(read_only=True)
+    class Meta:
+        model=GroupMessage
+        fields='__all__'
