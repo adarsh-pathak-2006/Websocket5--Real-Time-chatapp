466c17a92edb4a4fd761d77039e9b76484748067
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-24 15:30:51 +0530

    added whole chat logic...using http based request-response

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,10 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from chat.models import Chat, Conversation
 from whoisthis.serializers import UserSerializer
+from django.contrib.auth.models import User
 
 class ChatSerializer(ModelSerializer):
     user1=UserSerializer(read_only=True)
     user2=UserSerializer(read_only=True)
+    class Meta:
+        model=Chat
+        fields='__all__'
+
+class ChatCreationSerializer(ModelSerializer):
+    user2=PrimaryKeyRelatedField(queryset=User.objects.all())
     class Meta:
         model=Chat
         fields='__all__'
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from chat.serializers import ChatSerializer, ConversationSerializer, ChatCreationSerializer
+from chat.models import Chat, Conversation
+from rest_framework.response import Response
+from django.db.models import Q
 
-# Create your views here.
+
+class ChatAPI(APIView):
+    def get(self, request):
+        data=Chat.objects.filter(user1=request.user)
+        serial=ChatSerializer(data, many=True)
+        return Response(serial.data, status=200)
+
+    def post(self, request):
+        serial=ChatCreationSerializer(data=request.data)
+        if serial.is_valid():
+            serial.save(user1=request.user)
+            return Response(serial.data, status=201)
+        else:
+            return Response(serial.errors, status=400)
+
+class ConversationAPI(APIView):
+    def get(self, request, pk):
+        chat_data=get_object_or_404(Chat, Q(user1=request.user) | Q(user2=request.user), id=pk)
+        data=Conversation.objects.filter(chat=chat_data, user=request.user)
+        serial=ConversationSerializer(data, many=True)
+        return Response(serial.data, status=200)
