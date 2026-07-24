0e28ebb0a685219425def8e5efa148dc07dbcdce
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-24 11:51:05 +0530

    built chat models and linked them to admin panel

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from chat.models import Chat, Conversation
 
-# Register your models here.
+
+admin.site.register(Chat)
+admin.site.register(Conversation)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Chat(models.Model):
+    user1=models.ForeignKey(User, on_delete=models.CASCADE, related_name='who_is_talking')
+    user2=models.ForeignKey(User, on_delete=models.CASCADE, related_name='talking to who')
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints=[models.UniqueConstraint(fields=['user1', 'user2'], name='unique_chat_constraint')]
+
+    def __str__(self):
+        return self.user1.username
+        
+
+class Conversation(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    chat=models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='convo')
+    message=models.TextField()
+    time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
