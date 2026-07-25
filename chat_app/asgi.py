051e92cb5056ecc441b653dbdceea1099e59a599
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-25 16:13:20 +0530

    fixed daphne error in asgi while deployment

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -8,15 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat_app.middleware import JWTAuthMiddlewareStack
 from chat_app.routing import websocket_urlpatterns
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
 })
+#to push on github
